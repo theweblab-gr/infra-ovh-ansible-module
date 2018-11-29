86426ef64fab2b3b35c5db4e260649554207ead4
Emmanouil Karalakis <emannouil.karalakis@accenture.com>
2018-11-29 19:48:08 +0200

    groovy ansible

--- pipe.groovy
+++ pipe.groovy
@@ -13,5 +13,31 @@ pipeline {
                 sh "echo $PATH"
             }
         }
+        stage('ansible_build') {
+            steps {
+                echo "Hello Ansible!"
+                ansiblePlaybook('infra-ovh-ansible.yaml') {
+                    //inventoryPath('hosts.ini')
+                    //ansibleName('1.9.4')
+                    //limit('retry.limit')
+                    tags('ovh-servers-list')
+                    //skippedTags('three')
+                    //startAtTask('task')
+                   // credentialsId('credsid')
+                   // become(true)
+                   // becomeUser("user")
+                   // forks(6)
+                   // unbufferedOutput(false)
+                    colorizedOutput(true)
+                    disableHostKeyChecking(true)
+                    //additionalParameters('params')
+                    extraVars {
+                        extraVar ("application_key","value",true)
+                        extraVar ("application_secret","value",true)
+                        extraVar ("consumer_key","value",true)
+                    }
+                }
+            }
+        }
     }
 }

3ee4e9852c0cce458766ebeacfe55e42f834d930
Emmanouil Karalakis <emannouil.karalakis@accenture.com>
2018-11-29 20:25:33 +0200

    2

--- pipe.groovy
+++ pipe.groovy
@@ -15,16 +15,23 @@ pipeline {
         }
         stage('ansible_build') {
             steps {
-    ansiblePlaybook('path/playbook.yml') {
-        inventoryPath('hosts.ini')
-        ansibleName('1.9.4')
-        tags('one,two')
-        credentialsId('credsid')
-        become(true)
-        becomeUser("user")
+    ansibleAdHoc(String module, String command) {
+        ansibleName(String name)
+        inventoryPath(String path)
+        inventoryContent(String content, boolean dynamic = false)
+        credentialsId(String id)
+        hostPattern(String pattern)
+        become(boolean become = true)
+        becomeUser(String user = 'root')
+        sudo(boolean sudo = true)
+        sudoUser(String user = 'root')
+        forks(int forks = 5)
+        unbufferedOutput(boolean unbufferedOutput = true)
+        colorizedOutput(boolean colorizedOutput = false)
+        hostKeyChecking(boolean hostKeyChecking = false)
+        additionalParameters(String params)
         extraVars {
-            extraVar("key1", "value1", false)
-            extraVar("key2", "value2", true)
+            extraVar(String key, String value, boolean hidden)
         }
     }
 }

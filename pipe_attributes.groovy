90d969278e295dceeef8cb041e2705688cf0cdfa
Emmanouil Karalakis <emannouil.karalakis@accenture.com>
2018-12-05 00:10:10 +0200

    finaly EnvInjectJobProperty

--- pipe_attributes.groovy
+++ pipe_attributes.groovy
@@ -1,35 +1,45 @@
 #!groovy
 
 properties ([
-    buildDiscarder(logRotator(artifactDaysToKeepStr: "", artifactNumToKeepStr: "", daysToKeepStr: "15",
+       buildDiscarder(logRotator(artifactDaysToKeepStr: "", artifactNumToKeepStr: "", daysToKeepStr: "15",
                               numToKeepStr: "10")
                   ),
-    disableConcurrentBuilds(),
-    parameters([
-	[$class: 'NodeParameterDefinition',
-              name: 'Slave_Node',
-              allowedSlaves: ['Ansible_1'],
-              defaultSlaves: ['Ansible_1'],
-              description: 'Jenkins node where the job will run.',
-              nodeEligibility: [$class: 'AllNodeEligibility'],
-              triggerIfResult: 'multiSelectionDisallowed'
-       ],
+       disableConcurrentBuilds(),
        [$class: 'EnvInjectJobProperty',
+              info: [
+                     loadFilesFromMaster: false,
+                     propertiesContent:
+'''
+# Service variable to check whether this groovy pipeline has been configured
+IS_PIPELINE_CONFIGURED=true
+DRY_RUN=false
+''',
+                     secureGroovyScript: [classpath: [], sandbox: false, script: '']
+                     ],
               keepBuildVariables: true,
               keepJenkinsSystemVariables: true,
               on: true
        ],
-       booleanParam(
-              name: "BREAK_ON_LLD_ERRORS",
-              defaultValue: false,
-              description: "Break the build if errors are found in the Infrastructure LLD"),
-       credentials(
-              name: "Git_Creds",
-              required: false,
-              credentialType: "com.cloudbees.plugins.credentials.impl.CertificateCredentialsImpl",
-              defaultValue: "github_theweblab_ovh",
-              description: "Credentials for connecting to git."),
-    ]),
+       parameters([
+	       [$class: 'NodeParameterDefinition',
+                     name: 'Slave_Node',
+                     allowedSlaves: ['Ansible_1'],
+                     defaultSlaves: ['Ansible_1'],
+                     description: 'Jenkins node where the job will run.',
+                     nodeEligibility: [$class: 'AllNodeEligibility'],
+                     triggerIfResult: 'multiSelectionDisallowed'
+              ],
+              booleanParam(
+                     name: "BREAK_ON_LLD_ERRORS",
+                     defaultValue: false,
+                     description: "Break the build if errors are found in the Infrastructure LLD"),
+              credentials(
+                     name: "Git_Creds",
+                     required: false,
+                     credentialType: "com.cloudbees.plugins.credentials.impl.CertificateCredentialsImpl",
+                     defaultValue: "github_theweblab_ovh",
+                     description: "Credentials for connecting to git."),
+       ]),
 ])
 
 node(Slave_Node){

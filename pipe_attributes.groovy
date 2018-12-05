81c1a571b78654ec019e7f30c3f9ce32b664090b
Emmanouil Karalakis <emannouil.karalakis@accenture.com>
2018-12-05 19:39:15 +0200

    infra-ovh-ansible

--- pipe_attributes.groovy
+++ pipe_attributes.groovy
@@ -70,9 +70,11 @@ node(Slave_Node){
               sh "ls -al"
               checkout scm
               sh "ls -al"
-              ansiblePlaybook credentialsId: '${Target_Host_Creds}', colorized: true, disableHostKeyChecking: true, installation: 'Ansible_1', inventory: 'hosts', playbook: 'sample_playbook.yalm'
-              echo "Executing ansible playbook from sh"
-              sh "ANSIBLE_HOST_KEY_CHECKING=false ansible-playbook sample_playbook_local.yalm "
+              ansiblePlaybook colorized: true, disableHostKeyChecking: true, installation: 'Ansible_1', inventory: 'hosts', playbook: 'infra-ovh-ansible.yaml'
+
+              //ansiblePlaybook credentialsId: '${Target_Host_Creds}', colorized: true, disableHostKeyChecking: true, installation: 'Ansible_1', inventory: 'hosts', playbook: 'sample_playbook.yalm'
+              //echo "Executing ansible playbook from sh"
+              //sh "ANSIBLE_HOST_KEY_CHECKING=false ansible-playbook sample_playbook_local.yalm "
 
 
         }
